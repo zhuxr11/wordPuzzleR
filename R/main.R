7a0777aa96d5adc352a88100312bef1df50a46e9
zhuxr11 <zxr6@163.com>
2024-02-24 22:20:18 +0800

    Fix a bug in updating `best_hit_rate` (again).

--- R/main.R
+++ R/main.R
@@ -46,9 +46,8 @@ run_game <- function(mask_char = "_", verbose = TRUE, ...) {
         best_guess <- round_res[["iter"]]
       }
       total_unique_letters <- length(unique(split_word(round_res[["word"]])))
-      cur_hit_rate_value <-
       if ((is.na(best_hit_rate) == TRUE) ||
-          (round_res[["iter"]] / total_unique_letters > best_hit_rate)) {
+          (total_unique_letters / round_res[["iter"]] > best_hit_rate)) {
         best_hit <- c(hit = total_unique_letters, guess = round_res[["iter"]])
         best_hit_rate <- best_hit[["hit"]] / best_hit[["guess"]]
       }
